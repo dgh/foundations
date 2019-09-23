8a1c8710e5f6ed72d53d97099612b4e16d60e60f
David <david.holton@outlook.com>
2019-09-23 01:51:03 -0400

    Organized

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 import math
 from itertools import product
-
 from char import Char
 from alphabet import Alphabet
 from string import String
-
-
-alphabet = Alphabet([Char('0'), Char('1')])
-test_string = String([Char('0'), Char('1')], alphabet)
 
 def generate_nth_string(a, n):
 	if not n: return String([Char()], a)
@@ -16,5 +11,13 @@ def generate_nth_string(a, n):
 	i =  n + 1 - len(a) ** l # i = layer index + 1
 	return String(list(product(a, repeat=l))[i], a)
 
+binary_alpha = Alphabet([Char('0'), Char('1')])
+test_alpha = Alphabet([Char(c) for c in ['a', 'b', 'c', 'd', 'e']])
+string = String([Char('a'), Char('b')], test_alpha)
+
+print(test_alpha)
+print(string)
+
+print('generate_nth_string(binary_alpha, n):')
 for i in range(0, 31):
-	print('{}:\t{}'.format(i, generate_nth_string(alphabet, i)))
+	print('{}:\t{}'.format(i, generate_nth_string(binary_alpha, i)))
