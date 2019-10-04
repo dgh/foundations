21ff8ae564126dff137ede2ed68d0c8f3b8449ea
David <david.holton@outlook.com>
2019-10-04 00:31:02 -0400

    Completed Task #5
    Write the definition of a DFA that accepts no strings.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from itertools import product
 from char import Char
 from alphabet import Alphabet
 from string import String
+from dfa import DFA
 
 def generate_nth_string(a, n):
 	if not n: return String([Char()], a)
@@ -11,13 +12,9 @@ def generate_nth_string(a, n):
 	i =  n + 1 - len(a) ** l # i = layer index + 1
 	return String(list(product(a, repeat=l))[i], a)
 
-binary_alpha = Alphabet([Char('0'), Char('1')])
-test_alpha = Alphabet([Char(c) for c in ['a', 'b', 'c', 'd', 'e']])
-string = String([Char('a'), Char('b')], test_alpha)
+binary = Alphabet([Char('0'), Char('1')])
 
-print(test_alpha)
-print(string)
-
-print('generate_nth_string(binary_alpha, n):')
-for i in range(0, 31):
-	print('{}:\t{}'.format(i, generate_nth_string(binary_alpha, i)))
+no_strings_dfa = DFA('no_strings_dfa', binary,
+					 (lambda qi: False), 0,
+					 (lambda qi, c: False),
+					 (lambda qi: False))
