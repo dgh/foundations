e48fa123e185b46856f787e3c2d2890e9d60b54c
David <david.holton@outlook.com>
2019-11-17 11:46:25 -0500

    Completed Task #42
    Write a printer for regular expressions.

--- regex.py
+++ regex.py
@@ -12,22 +12,37 @@ class re_eps(regex):
 	def __init__(self):
 		pass
 
+	def __repr__(self):
+		return 'ε'
+
 class re_c(regex):
 	def __init__(self, c):
 		if isinstance(c, str):
 			c = Char(c)
 		self.c = c
 
+	def __repr__(self):
+		return self.c.__repr__()
+
 class re_u(regex):
 	def __init__(self, lc, rc):
 		self.l = lc
 		self.r = rc
+
+	def __repr__(self):
+		return f'{self.l}∪{self.r}'
 
 class re_cat(regex):
 	def __init__(self, lc, rc):
 		self.l = lc
 		self.r = rc
 
+	def __repr__(self):
+		return f'{self.l}◦{self.r}'
+
 class re_star(regex):
 	def __init__(self, r):
 		self.r = r
+
+	def __repr__(self):
+		return f'({self.r})*'
